bd555e161ff9213f416be1869b95adf6ffee42b3
Der Lude <lude@localhost.localdomain>
2023-06-09 11:53:46 +0200

    repo initialisiert

---
+++ mergesort.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+
+# Function to sort a list via mergesort
+
+
+def merge_sort(list_to_sort):
+    length = len(list_to_sort)
+    
+    # Exit condition because it is a recursive function
+    if (length > 1):
+    	
+    	# Divide the list in two sublists at the middle
+        mid = length // 2
+        left = list_to_sort[:mid]
+        right = list_to_sort[mid:]
+        
+        # Sort both sublists
+        merge_sort(left)
+        merge_sort(right)
+
+        index_left = 0
+        index_right = 0
+        index_list = 0
+        
+        # We sort until the end of one sublist is reached
+        while index_left < len(left) and \
+              index_right < len(right):
+            
+            # Comparison of elements and place the smaller value in the original list
+            if left[index_left] <= right[index_right]:
+                list_to_sort[index_list] = left[index_left]
+                index_left += 1
+            else:
+                list_to_sort[index_list] = right[index_right]
+                index_right += 1
+            index_list += 1
+        
+        # If one of our sublists is empty,
+        # we sort in the remaining elements from the other list
+        while index_left < len(left):
+            list_to_sort[index_list] = left[index_left]
+            index_left += 1
+            index_list += 1
+
+        while index_right < len(right):
+            list_to_sort[index_list] = right[index_right]
+            index_right += 1
+            index_list += 1
+
+
+# Create a list and plot it before and after sort
+my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+x = range(len(my_list))
+plt.plot(x, my_list)
+plt.show()
+merge_sort(my_list)
+plt.plot(x, my_list)
+plt.show()
